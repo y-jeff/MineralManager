e14a226b9c11be6db930a1eae724537ec3d5e4b0
Jeff-017 <y.jeffreyes@gmail.com>
2024-10-24 03:20:56 -0300

    Actualización de Models

--- MineralApp/admin.py
+++ MineralApp/admin.py
@@ -1,30 +1,38 @@
 from django.contrib import admin
 from .models import (
-    Area, Categoria, Articulo, Bodega, InventarioArticuloBodega, Capacitacion,
-    CargoTrabajador, Trabajador, HorarioTrabajo, JornadaTrabajador, TurnoTrabajador,
-    RegistroAsistenciaTrabajador, Asistencia, AsignacionArticulo, CapacitacionTrabajador,
-    JornadaEquipo, Panol, RetiroArticulo, RegistroEntrega, CustomUser
+    Area,
+    Categoria,
+    Articulo,
+    Bodega,
+    InventarioArticuloBodega,
+    MovimientoInventario,
+    Capacitacion,
+    CargoTrabajador,
+    Trabajador,
+    HorarioTrabajo,
+    JornadaTrabajador,
+    TurnoTrabajador,
+    RegistroAsistenciaTrabajador,
+    Asistencia,
+    CapacitacionTrabajador,
+    Panol,
+    RetiroArticulo,
+    RegistroEntrega,
+    Maquinaria,
+    RegistroCambios,
+    MantenimientoMaquinaria,
+    RetiroMaquinaria,
+    CustomUser  # Ya está registrado
 )
 
-# Registro de CustomUser con opciones adicionales para el panel de administración
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_approved', 'email_confirmed', 'is_admin')
-    list_filter = ('is_approved', 'email_confirmed', 'is_admin')
-    search_fields = ('username', 'email')
-    ordering = ('username',)
-    fieldsets = (
-        (None, {'fields': ('username', 'email', 'password')}),
-        ('Permisos', {'fields': ('is_approved', 'email_confirmed', 'is_admin', 'is_active')}),
-    )
 
-admin.site.register(CustomUser, CustomUserAdmin)
-
-# Registro de los demás modelos
+# Registrando los modelos en el administrador
 admin.site.register(Area)
 admin.site.register(Categoria)
 admin.site.register(Articulo)
 admin.site.register(Bodega)
 admin.site.register(InventarioArticuloBodega)
+admin.site.register(MovimientoInventario)
 admin.site.register(Capacitacion)
 admin.site.register(CargoTrabajador)
 admin.site.register(Trabajador)
@@ -33,9 +41,11 @@ admin.site.register(JornadaTrabajador)
 admin.site.register(TurnoTrabajador)
 admin.site.register(RegistroAsistenciaTrabajador)
 admin.site.register(Asistencia)
-admin.site.register(AsignacionArticulo)
 admin.site.register(CapacitacionTrabajador)
-admin.site.register(JornadaEquipo)
 admin.site.register(Panol)
 admin.site.register(RetiroArticulo)
 admin.site.register(RegistroEntrega)
+admin.site.register(Maquinaria)
+admin.site.register(RegistroCambios)
+admin.site.register(MantenimientoMaquinaria)
+admin.site.register(RetiroMaquinaria)
--- MineralApp/models.py
+++ MineralApp/models.py
@@ -1,168 +1,10 @@
 from django.db import models
 from django.core.mail import send_mail
 from django.conf import settings
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
-# Tabla: AREA
-class Area(models.Model):
-    nombre_area = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nombre_area
-
-# Tabla: CATEGORIA
-class Categoria(models.Model):
-    nombre_categoria_item = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nombre_categoria_item
-
-# Tabla: ARTICULOS
-class Articulo(models.Model):
-    categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT)
-    nombre_articulo = models.CharField(max_length=50)
-    descripcion_articulo = models.CharField(max_length=2000)
-
-    def __str__(self):
-        return self.nombre_articulo
-
-# Tabla: BODEGA
-class Bodega(models.Model):
-    nombre_bodega = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nombre_bodega
-
-# Tabla: INVENTARIO_ARTICULOS_BODEGA
-class InventarioArticuloBodega(models.Model):
-    articulo = models.ForeignKey(Articulo, on_delete=models.RESTRICT)
-    bodega = models.ForeignKey(Bodega, on_delete=models.RESTRICT)
-    cantidad_articulos = models.IntegerField()
-    en_bodega = models.BooleanField()
-
-    def __str__(self):
-        return f"{self.articulo} en {self.bodega}"
-
-# Tabla: CAPACITACION
-class Capacitacion(models.Model):
-    nombre_capacitacion = models.CharField(max_length=256)
-
-    def __str__(self):
-        return self.nombre_capacitacion
-
-# Tabla: CARGO_TRABAJADOR
-class CargoTrabajador(models.Model):
-    nombre_cargo = models.CharField(max_length=256)
-
-    def __str__(self):
-        return self.nombre_cargo
-
-# Tabla: TRABAJADORES
-class Trabajador(models.Model):
-    nombre_trabajador = models.CharField(max_length=256)
-    rut = models.CharField(max_length=9, primary_key=True)
-    horario = models.ForeignKey('HorarioTrabajo', on_delete=models.RESTRICT)
-    area = models.ForeignKey(Area, on_delete=models.RESTRICT)
-    cargo = models.ForeignKey(CargoTrabajador, on_delete=models.RESTRICT)
-    horas_esperadas_totales = models.IntegerField()
-
-    def __str__(self):
-        return self.nombre_trabajador
-
-# Tabla: HORARIO_TRABAJO
-class HorarioTrabajo(models.Model):
-    jornada_trabajador = models.ForeignKey('JornadaTrabajador', on_delete=models.RESTRICT)
-    turno_trabajador = models.ForeignKey('TurnoTrabajador', on_delete=models.RESTRICT)
-
-    def __str__(self):
-        return f"Jornada {self.jornada_trabajador} - Turno {self.turno_trabajador}"
-
-# Tabla: JORNADA_TRABAJADOR
-class JornadaTrabajador(models.Model):
-    tipo_jornada = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.tipo_jornada
-
-# Tabla: TURNO_TRABAJADOR
-class TurnoTrabajador(models.Model):
-    tipo_turno = models.CharField(max_length=256)
-
-    def __str__(self):
-        return self.tipo_turno
-
-# Tabla: REGISTRO_ASISTENCIA_TRABAJADOR
-class RegistroAsistenciaTrabajador(models.Model):
-    trabajador = models.ForeignKey(Trabajador, on_delete=models.RESTRICT)
-    asistencia = models.ForeignKey('Asistencia', on_delete=models.RESTRICT)
-
-    def __str__(self):
-        return f"Asistencia de {self.trabajador} - {self.asistencia.fecha_trabajada}"
-
-# Tabla: ASISTENCIA
-class Asistencia(models.Model):
-    fecha_trabajada = models.DateField()
-    hora_trabajada = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.fecha_trabajada} - {self.hora_trabajada} horas"
-
-# Tabla: ASIGNACION_ARTICULO
-class AsignacionArticulo(models.Model):
-    panol = models.ForeignKey('Panol', on_delete=models.RESTRICT)
-    inventario = models.ForeignKey(InventarioArticuloBodega, on_delete=models.RESTRICT)
-    cantidad_asignada_stock = models.IntegerField()
-
-    def __str__(self):
-        return f"Asignación en {self.panol} - {self.inventario.articulo}"
-
-# Tabla: CAPACITACION_TRABAJADOR
-class CapacitacionTrabajador(models.Model):
-    trabajador = models.ForeignKey(Trabajador, on_delete=models.RESTRICT)
-    capacitacion = models.ForeignKey(Capacitacion, on_delete=models.RESTRICT)
-    fecha_de_capacitacion = models.DateField()
-
-    def __str__(self):
-        return f"{self.capacitacion} - {self.trabajador}"
-
-# Tabla: JORNADA_EQUIPO
-class JornadaEquipo(models.Model):
-    tipo_jornada_equipo = models.CharField(max_length=256)
-
-    def __str__(self):
-        return self.tipo_jornada_equipo
-
-# Tabla: PANOL
-class Panol(models.Model):
-    nombre_panol = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nombre_panol
-
-# Tabla: RETIRO_ARTICULOS
-class RetiroArticulo(models.Model):
-    panol = models.ForeignKey(Panol, on_delete=models.RESTRICT)
-    trabajador = models.ForeignKey(Trabajador, on_delete=models.RESTRICT)
-    cantidad_retirada = models.IntegerField()
-    fecha_retiro_articulo = models.DateField()
-    motivo_retiro_articulo = models.CharField(max_length=2000)
-
-    def __str__(self):
-        return f"Retiro por {self.trabajador} - {self.cantidad_retirada}"
-
-# Tabla: REGISTRO_ENTREGA
-class RegistroEntrega(models.Model):
-    inventario = models.ForeignKey(InventarioArticuloBodega, on_delete=models.RESTRICT)
-    panol = models.ForeignKey(Panol, on_delete=models.RESTRICT)
-    fecha_registro_entrega = models.DateTimeField()
-    enviado_panol = models.BooleanField()
-    regresado_bodega = models.BooleanField()
-    fecha_registro_despacho = models.DateTimeField()
-
-    def __str__(self):
-        return f"Entrega en {self.panol} - {self.inventario.articulo}"
-
+# Custom user manager
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None):
         if not email:
@@ -191,6 +33,7 @@ class CustomUserManager(BaseUserManager):
         return user
 
 
+# Custom user model
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=60, unique=True)
@@ -226,3 +69,225 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     @property
     def is_staff(self):
         return self.is_admin
+
+
+# Tabla: AREA
+class Area(models.Model):
+    nombre_area = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre_area
+
+
+# Tabla: CATEGORIA
+class Categoria(models.Model):
+    nombre_categoria_item = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_categoria_item
+
+
+# Tabla: ARTICULOS
+class Articulo(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT)
+    nombre_articulo = models.CharField(max_length=50)
+    descripcion_articulo = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.nombre_articulo
+
+
+# Tabla: BODEGA
+class Bodega(models.Model):
+    nombre_bodega = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_bodega
+
+
+# Tabla: INVENTARIO_ARTICULOS_BODEGA
+class InventarioArticuloBodega(models.Model):
+    articulo = models.ForeignKey(Articulo, on_delete=models.RESTRICT)
+    bodega = models.ForeignKey(Bodega, on_delete=models.RESTRICT)
+    cantidad_articulos = models.IntegerField()
+    en_bodega = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.articulo} en {self.bodega}"
+
+
+# Tabla: MOVIMIENTOS DE BODEGA Y PAÑOL
+class MovimientoInventario(models.Model):
+    origen = models.ForeignKey(Bodega, on_delete=models.RESTRICT, related_name='movimiento_origen')
+    destino = models.ForeignKey(Bodega, on_delete=models.RESTRICT, related_name='movimiento_destino', null=True, blank=True)
+    articulo = models.ForeignKey(InventarioArticuloBodega, on_delete=models.RESTRICT)
+    fecha_movimiento = models.DateField()
+    cantidad = models.IntegerField()
+    motivo = models.CharField(max_length=200, null=True, blank=True)  # Motivo del retiro o movimiento
+
+    def __str__(self):
+        return f"Movimiento de {self.origen} a {self.destino} de {self.articulo}"
+
+
+# Tabla: CAPACITACION
+class Capacitacion(models.Model):
+    nombre_capacitacion = models.CharField(max_length=256)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()  # Fecha de finalización
+    es_renovable = models.BooleanField(default=False)  # Si requiere renovación
+    fecha_renovacion = models.DateField(null=True, blank=True)  # Fecha en que debe renovarse si es renovable
+
+    def __str__(self):
+        return self.nombre_capacitacion
+
+
+# Tabla: CARGO_TRABAJADOR
+class CargoTrabajador(models.Model):
+    nombre_cargo = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.nombre_cargo
+
+
+# Tabla: TRABAJADORES
+class Trabajador(models.Model):
+    nombre_trabajador = models.CharField(max_length=256)
+    rut = models.CharField(max_length=9, primary_key=True)
+    horario = models.ForeignKey('HorarioTrabajo', on_delete=models.RESTRICT)
+    area = models.ForeignKey(Area, on_delete=models.RESTRICT)
+    cargo = models.ForeignKey(CargoTrabajador, on_delete=models.RESTRICT)
+    horas_esperadas_totales = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre_trabajador
+
+
+# Tabla: HORARIO_TRABAJO
+class HorarioTrabajo(models.Model):
+    jornada_trabajador = models.ForeignKey('JornadaTrabajador', on_delete=models.RESTRICT)
+    turno_trabajador = models.ForeignKey('TurnoTrabajador', on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return f"Jornada {self.jornada_trabajador} - Turno {self.turno_trabajador}"
+
+
+# Tabla: JORNADA_TRABAJADOR
+class JornadaTrabajador(models.Model):
+    tipo_jornada = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.tipo_jornada
+
+
+# Tabla: TURNO_TRABAJADOR
+class TurnoTrabajador(models.Model):
+    tipo_turno = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.tipo_turno
+
+
+# Tabla: REGISTRO_ASISTENCIA_TRABAJADOR
+class RegistroAsistenciaTrabajador(models.Model):
+    trabajador = models.ForeignKey(Trabajador, on_delete=models.RESTRICT)
+    asistencia = models.ForeignKey('Asistencia', on_delete=models.RESTRICT)
+    observaciones = models.CharField(max_length=256, blank=True, null=True)
+
+    def __str__(self):
+        return f"Asistencia de {self.trabajador} - {self.asistencia.fecha_trabajada}"
+
+
+# Tabla: ASISTENCIA
+class Asistencia(models.Model):
+    fecha_trabajada = models.DateField()
+    hora_trabajada = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.fecha_trabajada} - {self.hora_trabajada} horas"
+
+
+# Tabla: CAPACITACION_TRABAJADOR
+class CapacitacionTrabajador(models.Model):
+    trabajador = models.ForeignKey(Trabajador, on_delete=models.RESTRICT)
+    capacitacion = models.ForeignKey(Capacitacion, on_delete=models.RESTRICT)
+    fecha_de_capacitacion = models.DateField()
+    renovacion_fecha = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.capacitacion} - {self.trabajador}"
+
+
+# Tabla: PANOL
+class Panol(models.Model):
+    nombre_panol = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_panol
+
+
+# Tabla: RETIRO_ARTICULOS
+class RetiroArticulo(models.Model):
+    panol = models.ForeignKey(Panol, on_delete=models.RESTRICT)
+    trabajador = models.ForeignKey(Trabajador, on_delete=models.RESTRICT)
+    cantidad_retirada = models.IntegerField()
+    fecha_retiro_articulo = models.DateField()
+    motivo_retiro_articulo = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return f"Retiro por {self.trabajador} - {self.cantidad_retirada}"
+
+
+# Tabla: REGISTRO_ENTREGA
+class RegistroEntrega(models.Model):
+    inventario = models.ForeignKey(InventarioArticuloBodega, on_delete=models.RESTRICT)
+    panol = models.ForeignKey(Panol, on_delete=models.RESTRICT)
+    fecha_registro_entrega = models.DateTimeField()
+    enviado_panol = models.BooleanField()
+    regresado_bodega = models.BooleanField()
+    fecha_registro_despacho = models.DateTimeField()
+
+    def __str__(self):
+        return f"Entrega en {self.panol} - {self.inventario.articulo}"
+
+# Tabla: MAQUINARIAS
+class Maquinaria(models.Model):
+    nombre_maquinaria = models.CharField(max_length=100)
+    fecha_adquisicion = models.DateField()
+    estado = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_maquinaria
+
+
+# Tabla: REGISTRO_CAMBIOS
+class RegistroCambios(models.Model):
+    usuario = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    tabla_afectada = models.CharField(max_length=100)
+    campo_modificado = models.CharField(max_length=100)
+    valor_anterior = models.CharField(max_length=255, null=True, blank=True)
+    valor_nuevo = models.CharField(max_length=255)
+    fecha_modificacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cambio en {self.tabla_afectada}: {self.campo_modificado}"
+
+
+# Tabla: MANTENIMIENTO_MAQUINARIA
+class MantenimientoMaquinaria(models.Model):
+    maquinaria = models.ForeignKey(Maquinaria, on_delete=models.CASCADE)
+    fecha_mantenimiento = models.DateField()
+    descripcion = models.TextField()
+    realizado_por = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Mantenimiento de {self.maquinaria} el {self.fecha_mantenimiento}"
+
+
+# Tabla: RETIRO_MAQUINARIA
+class RetiroMaquinaria(models.Model):
+    maquinaria = models.ForeignKey(Maquinaria, on_delete=models.CASCADE)
+    fecha_retiro = models.DateField()
+    motivo = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"Retiro de {self.maquinaria} el {self.fecha_retiro}"
